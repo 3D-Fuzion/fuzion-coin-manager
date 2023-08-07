4abf9193485e6cf21c66ef67780b592a3ff9e820
Guilherme-Vianna <mataveli91@gmail.com>
2023-08-07 16:00:33 -0300

    feat: Little Changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,12 @@ import { Login } from './components/Login';
 import { Input } from './components/Input';
 import axios from 'axios';
 function App() {
-  const logIn = async () => { 
-    var response = await axios.get('http://localhost:3000/user')
+  const logIn = async (email, password) => { 
+    var body = { 
+      email,
+      password
+    }
+    var response = await axios.post('http://localhost:3000/login', body)
     console.log(response);
   }
 
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,17 +1,30 @@
+import { useState } from "react"
 import "../css/Input.css"
 
-export function Input (props) { 
+export function Input () { 
+  const [userData, setUserData] = useState({
+    email: '',
+    password: ''
+  })
+  
+
+  const handleChange = (event) => { 
+    setUserData(event.target.value) 
+  }
+
   return( 
     <div class="container-sm text row">
     <div class='col'/>
     <div class='col-8'>
-      <label for="email" class="form-label">Email</label>
-      <input type="username" class='form-control' id="email"></input>
+    <form>
+    <label for="email" class="form-label">Email</label>
+      <input type="username" class='form-control' id="email" onChange={handleChange}></input>
       <label for="password" class="form-label">Senha</label>
       <input type="password" class='form-control' id="password"></input>  
       <div> 
-        <button type="button" class="btn btn-primary" onClick={props.method}>Acessar</button>
+        <button type="button" class="btn btn-primary">Acessar</button>
       </div>
+    </form>
     </div> 
     <div class='col'/>
     </div>
